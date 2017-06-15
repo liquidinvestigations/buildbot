2f83fc57a89c1f36cba0f3a82627b98bb314df96
Alex Morega <alex@grep.ro>
2017-06-15 12:39:12 +0000

    assume the build script will be run as root

--- shared/scripts/build_odroid_image.sh
+++ shared/scripts/build_odroid_image.sh
@@ -6,34 +6,34 @@ TARGET=/mnt/target
 TEMPDIR=/tmp
 OUTPUT=/mnt/shared/output
 
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt install -y ansible git pv
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt install -y ansible git pv
 
 git clone https://github.com/liquidinvestigations/setup $SETUPDIR
 
 curl https://liquidinvestigations.org/images/ubuntu64-16.04-minimal-odroid-c2-20160815-4G.img.xz | xzcat > $TEMPDIR/odroid-c2.img
 
-sudo losetup /dev/loop0 $TEMPDIR/odroid-c2.img -o 135266304
-sudo mkdir -p $TARGET
-sudo mount /dev/loop0 $TARGET
-sudo mount --bind /proc $TARGET/proc
-echo "nameserver 8.8.8.8" | sudo tee $TARGET/etc/resolv.conf
+losetup /dev/loop0 $TEMPDIR/odroid-c2.img -o 135266304
+mkdir -p $TARGET
+mount /dev/loop0 $TARGET
+mount --bind /proc $TARGET/proc
+echo "nameserver 8.8.8.8" > $TARGET/etc/resolv.conf
 
-sudo apt-get install -qq qemu-user-static binfmt-support
-sudo cp $(which qemu-arm-static) $TARGET/usr/bin
+apt-get install -qq qemu-user-static binfmt-support
+cp $(which qemu-arm-static) $TARGET/usr/bin
 
-sudo chroot $TARGET apt-get update
-sudo chroot $TARGET apt-get install -y python
-sudo chroot $TARGET apt-get clean
+chroot $TARGET apt-get update
+chroot $TARGET apt-get install -y python
+chroot $TARGET apt-get clean
 
 cd $SETUPDIR/ansible
 touch vars/config.yml
-sudo ansible-playbook board_chroot.yml
+ansible-playbook board_chroot.yml
 
-sudo rm $TARGET/usr/bin/qemu-arm-static
-sudo umount $TARGET
-sudo losetup -d /dev/loop0
+rm $TARGET/usr/bin/qemu-arm-static
+umount $TARGET
+losetup -d /dev/loop0
 
 mkdir -p $OUTPUT
 pv < $TEMPDIR/odroid-c2.img | xz > $OUTPUT/odroid-c2-liquid.img.xz
