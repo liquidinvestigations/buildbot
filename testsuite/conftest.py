9106f186a4cda1a38692363b806ff1b3e2425735
Alex Morega <alex@grep.ro>
2017-11-11 19:41:26 +0100

    use existing cloud image to run tests quickly

--- testsuite/conftest.py
+++ testsuite/conftest.py
@@ -1,5 +1,70 @@
 import sys
+from contextlib import contextmanager
+from tempfile import TemporaryDirectory
 from pathlib import Path
+import pytest
 
 repo = Path(__file__).resolve().parent.parent
 sys.path[:0] = [str(repo)]
+
+import factory as factory_module
+
+default_paths = factory_module.paths
+
+
+def pytest_addoption(parser):
+    parser.addoption('--image', help="use existing image for tests")
+
+
+@contextmanager
+def monkeypatcher():
+    from _pytest.monkeypatch import MonkeyPatch
+    mocks = MonkeyPatch()
+    try:
+        yield mocks
+    finally:
+        mocks.undo()
+
+
+@contextmanager
+def tmpdir_factory():
+    with TemporaryDirectory() as tmp:
+        tmp_repo = Path(tmp)
+
+        with monkeypatcher() as mocks:
+
+            class FactoryWrapper:
+
+                def __init__(self):
+                    self.images = tmp_repo / 'images'
+                    self.images.mkdir()
+
+                    self.shared = tmp_repo / 'shared'
+                    self.shared.mkdir()
+
+                    tmp_paths = factory_module.Paths(tmp_repo)
+                    mocks.setattr(factory_module, 'paths', tmp_paths)
+                    self.main = factory_module.main
+
+            yield FactoryWrapper()
+
+
+@pytest.fixture(scope='session')
+def cloud_image(pytestconfig):
+    image_name = pytestconfig.getoption('--image')
+
+    if image_name:
+        yield default_paths.IMAGES / image_name
+
+    else:
+        with tmpdir_factory() as factory:
+            factory.main(['prepare-cloud-image'])
+            [image] = factory.images.iterdir()
+            yield image
+
+
+@pytest.fixture
+def factory(cloud_image):
+    with tmpdir_factory() as factory:
+        (factory.images / cloud_image.name).symlink_to(cloud_image)
+        yield factory
--- testsuite/test_prepare_cloud_image.py
+++ testsuite/test_prepare_cloud_image.py
@@ -1,32 +1,13 @@
-from pathlib import Path
-from tempfile import TemporaryDirectory
-import pytest
-import factory
+def test_run_touch(factory):
+    hello = factory.shared / 'hello'
+    with hello.open('w', encoding='utf8') as f:
+        f.write('#!/bin/sh\ntouch /mnt/shared/world.txt\n')
+    hello.chmod(0o755)
 
+    factory.main([
+        'run',
+        '--share', '{}:/mnt/shared'.format(factory.shared),
+        '/mnt/shared/hello',
+    ])
 
-@pytest.fixture
-def run_factory(monkeypatch):
-    with TemporaryDirectory() as tmp:
-        repo = Path(tmp)
-        (repo / 'images').mkdir()
-        monkeypatch.setattr(factory, 'repo', repo)
-        yield factory.main
-
-
-def test_prepare_cloud_image(run_factory):
-    run_factory(['prepare-cloud-image'])
-
-    with TemporaryDirectory() as tmp:
-        shared = Path(tmp)
-        hello = shared / 'hello'
-        with hello.open('w', encoding='utf8') as f:
-            f.write('#!/bin/sh\ntouch /mnt/shared/world.txt\n')
-        hello.chmod(0o755)
-
-        run_factory([
-            'run',
-            '--share', '{}:/mnt/shared'.format(shared),
-            '/mnt/shared/hello',
-        ])
-
-        assert (shared / 'world.txt').is_file()
+    assert (factory.shared / 'world.txt').is_file()
